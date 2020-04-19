fac49c0a0c4ff44e38b96c23846bb285f1e1964e
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-04-19 16:56:22 +0000

    Added problem 441 and started updating HTML

---
+++ easy/441/solution.js
@@ -0,0 +1,20 @@
+//  @param {number} n
+//  @return {string[]}
+
+const fizBuzz = (n) => {
+  const output = [];
+  for (let index = 1; index <= n; index++) {
+    if (index % 15 === 0) {
+      output.push('FizzBuzz');
+    } else if (index % 3 === 0) {
+      output.push('Fizz');
+    } else if (index % 5 === 0) {
+      output.push('Buzz');
+    } else {
+      output.push(index.toString());
+    }
+  }
+  return output;
+};
+
+export default fizBuzz;
---
+++ easy/441/testcases.js
@@ -0,0 +1,95 @@
+import fizBuzz from './solution.js';
+
+const body = document.querySelector('body');
+const startButton = document.querySelector('button');
+const main = document.querySelector('main');
+const passed = document.createElement('h2');
+const failed = document.createElement('h2');
+passed.classList.add('smooth-transition');
+failed.classList.add('smooth-transition');
+main.append(passed);
+main.append(failed);
+
+let counter = 0;
+const results = {
+  passed: 0,
+  failed: 0
+}
+
+const testCases = [
+  [
+    1,
+    ["1"]
+  ],
+  [
+    10,
+    ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
+  ],
+  [
+    27,
+    ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz"]
+  ],
+  [
+    3,
+    ["1", "2", "Fizz"]
+  ],
+  [
+    5,
+    ["1", "2", "Fizz", "4", "Buzz"]
+  ],
+  [
+    105,
+    ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz", "31", "32", "Fizz", "34", "Buzz", "Fizz", "37", "38", "Fizz", "Buzz", "41", "Fizz", "43", "44", "FizzBuzz", "46", "47", "Fizz", "49", "Buzz", "Fizz", "52", "53", "Fizz", "Buzz", "56", "Fizz", "58", "59", "FizzBuzz", "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "Buzz", "71", "Fizz", "73", "74", "FizzBuzz", "76", "77", "Fizz", "79", "Buzz", "Fizz", "82", "83", "Fizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz", "91", "92", "Fizz", "94", "Buzz", "Fizz", "97", "98", "Fizz", "Buzz", "101", "Fizz", "103", "104", "FizzBuzz"]
+  ]
+];
+
+const buildErrorSentence = (testCase, result, itemCount) => {
+  const containingDiv = document.createElement('div');
+  const mainHeading = document.createElement('h3');
+  const input = document.createElement('p');
+  const expectedResultHeading = document.createElement('p');
+  const expectedResult = document.createElement('p');
+  const outputHeading = document.createElement('p');
+  const output = document.createElement('p');
+
+  mainHeading.innerText = `Failed Test Case ${itemCount + 1}`;
+  input.innerText = `input: ${testCase[0]}`;
+  expectedResultHeading.innerText = 'Expcted: '
+  expectedResult.innerText = `[${testCase[1]}]`;
+  outputHeading.innerText = 'Output: '
+  output.innerText = `[${result}]`;
+  expectedResult.classList.add('overflow-x-scroll');
+  output.classList.add('overflow-x-scroll');
+
+  containingDiv.append(mainHeading, input, expectedResultHeading, expectedResult, outputHeading, output);
+  containingDiv.classList.add('error-box')
+  main.append(containingDiv);
+}
+
+startButton.addEventListener('click', () => {
+  const delayTest = setInterval(() => {
+    if (counter < testCases.length) {
+      const result = fizBuzz(testCases[counter][0], testCases[counter][1]);
+      if (result.toString() === testCases[counter][1].toString()) {
+        if (!body.classList.contains('all-good') && !body.classList.contains('errors')) {
+          body.classList.add('all-good');
+        }
+        results.passed++;
+      } else {
+        if (!body.classList.contains('errors')) {
+          if (body.classList.contains('all-good')) {
+            body.classList.remove('all-good');
+          }
+          body.classList.add('errors');
+        }
+        results.failed++;
+        buildErrorSentence(testCases[counter], result, counter)
+      }
+      counter++;
+    } else {
+      clearInterval(delayTest);
+    }
+    passed.innerText = `${results.passed} ${results.passed === 1 ? 'Test' : 'Tests'} Passed`;
+    failed.innerText = `${results.failed} ${results.failed === 1 ? 'Test' : 'Tests'} Failed`;
+  }, 50);
+});
