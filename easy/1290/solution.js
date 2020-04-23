7ba1101ea43c215fd8479d153470809913a4cc4b
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-04-23 01:14:02 +0000

    Added solution to solution.js

--- easy/1290/solution.js
+++ easy/1290/solution.js
@@ -1,25 +1,19 @@
-// @param {number[]} arr1
-// @param {number[]} arr2
-// @return {number[]}
+// Definition for singly-linked list.
+// function ListNode(val) {
+//   this.val = val;
+//   this.next = null;
+// }
+// @param {ListNode} head
+// @return {number}
 
-const relativeSortArray = (arr1, arr2) => {
-  const output = [];
-  let arr1Copy = JSON.parse(JSON.stringify(arr1));
-
-  for (let arr2Index = 0; arr2Index < arr2.length; arr2Index++) {
-    for (let arr1Index = 0; arr1Index < arr1Copy.length;) {
-      if (arr1Copy[arr1Index] === arr2[arr2Index]) {
-        output.push(arr1Copy.splice(arr1Index, 1)[0]);
-      } else {
-        arr1Index++;
-      }
-    }
+const getDecimalValue = head => {
+  let binaryString = '';
+  let currentNode = head;
+  while (currentNode) {
+    binaryString += currentNode.val;
+    currentNode = currentNode.next;
   }
-
-  arr1Copy = arr1Copy.sort((first, second) => first - second);
-  output.push(...arr1Copy);
-
-  return output;
+  return parseInt(binaryString, 2)
 };
 
 export default relativeSortArray;
